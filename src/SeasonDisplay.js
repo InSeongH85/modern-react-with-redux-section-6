a113cfddd8609b3c48727d40ec26325ccbeb90bc
tanbbang01@gmail.com <tanbbang01@gmail.com>
2021-01-04 22:42:19 +0900

    SeasonDisplay Component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month <= 2 ||  month === 11) {
+    return lat > 0 ? 'winter' : 'spring'
+  } else if (month >= 3 && month <= 6) {
+    return lat > 0 ? 'spring' : 'summer'
+  } else if (month >=7 && month <= 9) {
+    return lat > 0 ? 'summer' : 'fall'
+  } else if (month >= 9 && month <= 10) {
+    return lat > 0 ? 'fall' : 'winter'
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth())
+
+  console.log(season);
+  
   return <div>Season Display</div>;
 };
 
